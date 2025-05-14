6db6c59595da1990c891c2767391bb63ac2c6ec2
AoEiuV020 <aoeiuv020@gmail.com>
2025-05-15 00:18:20 +0800

    create ffi

---
+++ example/android/app/src/main/kotlin/com/aoeiuv020/flutter_go_caller_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.aoeiuv020.flutter_go_caller_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Classes/flutter_go_caller.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../flutter_go_caller.podspec for more information.
+#include "../../src/flutter_go_caller.c"
